0b40b45d99d171fb639e7353fd05588d6c687cb3
MajorAmari <jmari@duck.com>
2024-05-12 19:52:40 +0300

    feat: :alien: add delete event endpoint

--- rest-api/routes/events.go
+++ rest-api/routes/events.go
@@ -84,5 +84,27 @@ func updateEvent(context *gin.Context) {
 		return
 	}
 
-	context.JSON(http.StatusCreated, gin.H{"message": "Event updated successfully", "event": updatedEvent})
+	context.JSON(http.StatusOK, gin.H{"message": "Event updated successfully", "event": updatedEvent})
+}
+
+func deleteEvent(context *gin.Context) {
+	eventID, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event id."})
+		return
+	}
+
+	event, err := models.GetEventByID(eventID)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch event."})
+		return
+	}
+
+	err = event.Delete()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not delete the event."})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Event deleted successfully"})
 }
