47bed7adc6788a841c9041ebf55348065cf3e666
MajorAmari <jmari@duck.com>
2024-05-13 06:42:01 +0300

    build: add .env to build release

--- rest-api/main.go
+++ rest-api/main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"os"
+
 	"elbolaky.com/rest-api/db"
 	"elbolaky.com/rest-api/routes"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		panic("Error loading .env file")
+	}
+	gin.SetMode(os.Getenv("GIN_MODE"))
+
 	db.InitDB()
 	server := gin.Default()
 
